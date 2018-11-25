a239d5fc603e238a9b87e86cc55f59c205af1468
karuppusamy.p <pranav.karuppusamy@gmail.com>
2018-11-25 16:15:50 +0530

    Updates

---
+++ precache-manifest.edfd26b6e3a3c660e5ebd2daa49cf512.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73eece3d62d6f8bd1dc5",
+    "url": "/iceagemovie/static/js/runtime~main.73eece3d.js"
+  },
+  {
+    "revision": "ac0e211dff6b0601cc7b",
+    "url": "/iceagemovie/static/js/main.ac0e211d.chunk.js"
+  },
+  {
+    "revision": "81399522ec27a46ddcf6",
+    "url": "/iceagemovie/static/js/1.81399522.chunk.js"
+  },
+  {
+    "revision": "ac0e211dff6b0601cc7b",
+    "url": "/iceagemovie/static/css/main.253b314c.chunk.css"
+  },
+  {
+    "revision": "03014b130924a2fd962babf20b5d78ac",
+    "url": "/iceagemovie/index.html"
+  }
+];
